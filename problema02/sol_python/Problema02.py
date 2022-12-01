67ed5c323b6fd351e4a6b8dd915bc61487701eee
ljmora004 <115653442+ljmora004@users.noreply.github.com>
2022-12-01 18:14:22 -0500

    Trabajo Final Bim1

--- problema02/sol_python/Problema02.py
+++ problema02/sol_python/Problema02.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 precioTraje = float(input("Ingrese el precio unitario del traje: "))
 cantidadTraje = int(input("Ingrese la cantidad de trajes: "))
 valorTotal = 0
@@ -22,27 +21,3 @@ valorTotal = subTotal - descuento
 print ("El subtotal del traje es de: $", precioTraje)
 print ("El descuento que se aplica es de: $", descuento)
 print ("El valor final a pagar es de: $", valorTotal)
-=======
-cantidadTraje = int(input("Ingrese la cantidad de trajes: "))
-precioTraje = float(input("Ingrese el precio unitario del traje: "))
-valorTotal = 0
-descuento = 0
-
-
-if cantidadTraje == 1:
-    descuento = (precioTraje * 20) / 100
-else:
-    if cantidadTraje == 2:
-        descuento = (precioTraje * 25)/ 100
-    else:
-        if cantidadTraje == 3:
-            descuento = (precioTraje * 40)/ 100
-        else:
-            if cantidadTraje > 3:
-                descuento = (precioTraje * 50) / 100
-valorTotal = precioTraje - descuento
-
-print ("El subtotal del traje es de: " + precioTraje)
-print ("El descuento que se aplica es de: " + descuento)
-print ("El valor final a pagar es de: " + valorTotal)
->>>>>>> cf6e067ebc047f319a7bd0f51a36962516d0b716
